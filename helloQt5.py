87d5cee59c08fe61724deb56247879dd1fc9fb7b
ABHISHEK UPADHYAY <abhisheku722@gmail.com>
2021-09-01 19:33:30 +0530

    commit

--- helloQt5.py
+++ helloQt5.py
@@ -6,7 +6,7 @@ from PyQt5.QtWidgets import QWidget
 app = QApplication(sys.argv)
 
 window = QWidget()
-window.setWindowTitle('PyQt5t App')
+window.setWindowTitle('PyQt5 App')
 window.setGeometry(100, 100, 280, 80)
 window.move(60, 15)
 helloMsg = QLabel('<h1> Hello World!</h1>', parent = window)
---
+++ shubhamtest.py
@@ -0,0 +1,3 @@
+age = 70
+salary = 2543.50
+print("this is my age "+ str(age)+ " and my salary is "+str(salary))
