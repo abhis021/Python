4dc7b80470d18519dffc3e0e775f836c42aebbba
ABHISHEK UPADHYAY <43817822+abhis021@users.noreply.github.com>
2024-10-18 16:45:59 +0530

    other than helloworld
    weird not weird

--- hello world.py
+++ hello world.py
@@ -1,1 +1,15 @@
-print("hello world")
+import math
+import os
+import random
+import re
+import sys
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    if n %2 != 0:
+        print("Weird")
+        print ("Not Weird")
+    elif 6 <=n <=20:
+        print("Weird")
+    else:
+        print("Not Weird")
