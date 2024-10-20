37b3a4e0f249b8b7514c53d1fcf830f57e6a96ce
ABHISHEK UPADHYAY <43817822+abhis021@users.noreply.github.com>
2024-10-20 13:07:42 +0530

    commit baby

--- 200 june.py
+++ 200 june.py
@@ -6,5 +6,5 @@ for x in thistuple:
 thistuple = ("apple", "banana", "cherry")
 if "apple" in thistuple:
     print("Yes,'apple' is in the fruits tuple.")
-thistuple("apple", "banana", "cherry")
+    thistuple("apple", "banana", "cherry")
 print(len(thistuple))
