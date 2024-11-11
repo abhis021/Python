c8b628dfd638f29cc9181c407385640a22e40ace
ABHISHEK UPADHYAY <43817822+abhis021@users.noreply.github.com>
2024-11-11 08:44:32 +0530

    lol

--- hello world.py
+++ hello world.py
@@ -1,2 +1,10 @@
-abc="bond"
-print("Hello", abc)
+# abc="bond"
+# print("Hello", abc)
+count =0
+sum=0
+print("before",count,sum)
+for value in [9,41,12,3,74,15]:
+    count+=1
+    sum+=value
+    print(count,sum,value)
+print('after',count,sum,sum/count)
